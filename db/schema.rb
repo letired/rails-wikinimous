e90b953e0136adc9bcc261a0e3294be77a0e0090
Spencer Bray <spencerjohnbray@gmail.com>
2017-04-18 16:29:29 +0200

    built a scaffold, added markdown support

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418124953) do
+ActiveRecord::Schema.define(version: 20170418133418) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
